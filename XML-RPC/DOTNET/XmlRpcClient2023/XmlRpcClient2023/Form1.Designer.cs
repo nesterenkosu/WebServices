ee9705db6f0902b491ed4a1aa48022307127612b
SU-Nesterenko <55326271+SU-Nesterenko@users.noreply.github.com>
2023-10-20 13:50:47 +0500

    Добавлены примеры XML-RPC сервера и клиента на DOTNET

---
+++ XML-RPC/DOTNET/XmlRpcClient2023/XmlRpcClient2023/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace XmlRpcClient2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_Name = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_Age = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_Result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_Name
+            // 
+            this.lb_Name.AutoSize = true;
+            this.lb_Name.Location = new System.Drawing.Point(47, 44);
+            this.lb_Name.Name = "lb_Name";
+            this.lb_Name.Size = new System.Drawing.Size(35, 17);
+            this.lb_Name.TabIndex = 0;
+            this.lb_Name.Text = "Имя";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_Age
+            // 
+            this.tb_Age.Location = new System.Drawing.Point(124, 72);
+            this.tb_Age.Name = "tb_Age";
+            this.tb_Age.Size = new System.Drawing.Size(100, 22);
+            this.tb_Age.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Возраст";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lb_Result
+            // 
+            this.lb_Result.AutoSize = true;
+            this.lb_Result.Location = new System.Drawing.Point(47, 164);
+            this.lb_Result.Name = "lb_Result";
+            this.lb_Result.Size = new System.Drawing.Size(46, 17);
+            this.lb_Result.TabIndex = 0;
+            this.lb_Result.Text = "label1";
+            this.lb_Result.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 304);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_Age);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_Result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_Name;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_Age;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_Result;
+    }
+}
+
---
+++ XML-RPC/DOTNET/XmlRpcClient2023/XmlRpcClient2023/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CookComputing.XmlRpc;
+
+
+namespace XmlRpcClient2023
+{    
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IDemoServiceProxy proxy = XmlRpcProxyGen.Create<IDemoServiceProxy>();
+            proxy.HelloUser("Vasya", 19);
+        }
+    }
+
+    [XmlRpcUrl("https://localhost:44302/Server.ashx")]
+    public interface IDemoServiceProxy : IXmlRpcProxy
+    {
+        [XmlRpcMethod("demoservice::hellouser")]
+        string HelloUser(string name, int age);
+    }
+}
